10f4debc8c3e3971810141a377c6beb8239cc1e6
Agathe Vaisse <agatesse@hotmail.fr>
2019-02-23 17:25:39 +0100

    Initialize project

---
+++ src/main/java/com/projects/breakingbook/BreakingBookApplication.java
@@ -0,0 +1,13 @@
+package com.projects.breakingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakingBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakingBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/breakingbook/BreakingBookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projects.breakingbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BreakingBookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
