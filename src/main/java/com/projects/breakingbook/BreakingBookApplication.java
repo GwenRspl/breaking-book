fa4504c4087dbf1514b76d1f77458274c9810a91
GwenRspl <gwenaelle.rispal@gmail.com>
2019-04-15 13:45:36 +0200

    add BookDTO

--- src/main/java/com/projects/breakingbook/BreakingBookApplication.java
+++ src/main/java/com/projects/breakingbook/BreakingBookApplication.java
@@ -1,7 +1,9 @@
 package com.projects.breakingbook;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BreakingBookApplication {
@@ -10,4 +12,8 @@ public class BreakingBookApplication {
 		SpringApplication.run(BreakingBookApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
---
+++ src/main/java/com/projects/breakingbook/exposition/DTO/UserDTO.java
@@ -0,0 +1,4 @@
+package com.projects.breakingbook.exposition.DTO;
+
+public class UserDTO {
+}
