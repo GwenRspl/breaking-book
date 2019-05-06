d9205988e9644dea35b75f8fe2551c901311c2c5
GwenRspl <gwenaelle.rispal@gmail.com>
2019-05-06 18:40:48 +0200

    add sign in feature and token storage

--- src/main/ui/src/app/authentication/sign-in/sign-in.page.ts
+++ src/main/ui/src/app/authentication/sign-in/sign-in.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {SignInInfo} from '../sign-in-info';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../service/authentication.service';
+import {ToastController} from '@ionic/angular';
+import {TokenStorageService} from '../token-storage.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +12,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.page.scss'],
 })
 export class SignInPage implements OnInit {
-
-  constructor() { }
+  signInForm: FormGroup;
+  submitted: boolean;
+  isLoginFailed = false;
+  private signInInfo: SignInInfo;
+  
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authService: AuthenticationService,
+              private toastCtrl: ToastController,
+              private tokenStorage: TokenStorageService) { }
 
   ngOnInit() {
+    this.initSignInForm();
   }
 
+  initSignInForm(){
+    this.signInForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  get f() {
+    return this.signInForm.controls;
+  }
+
+  signInUser() {
+    this.submitted = true;
+    if(this.signInForm.invalid) {
+      console.log('invalid form');
+      return;
+    }
+    this.signInInfo = new SignInInfo(this.signInForm.value.username, this.signInForm.value.password);
+    this.authService.attemptAuthentication(this.signInInfo).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.token);
+        this.tokenStorage.saveUsername(data.username);
+        this.tokenStorage.saveAuthorities(data.authorities);
+        this.isLoginFailed = false;
+        this.router.navigateByUrl('/library');
+
+      },
+      error => {
+        console.log(error);
+        this.isLoginFailed = true;
+        this.presentErrorToast('Invalid username or password.');
+      }
+    )
+
+  }
+
+  goToSignUp() {
+    this.router.navigateByUrl('/sign-up');
+  }
+
+  async presentErrorToast(message:string) {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 10000,
+      color: 'danger',
+      showCloseButton: true
+    });
+    toast.present();
+  }
 }
