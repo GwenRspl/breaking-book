a3f191ab298ad207cf49403ef43c38a7fabf6620
GwenRspl <gwenaelle.rispal@gmail.com>
2019-05-20 19:43:34 +0200

    implement filters of ownership, status and ratings

--- src/main/ui/src/app/library/library.page.ts
+++ src/main/ui/src/app/library/library.page.ts
@@ -5,11 +5,24 @@ import {BooksService} from './services/books.service';
 import {TokenStorageService} from '../authentication/services/token-storage.service';
 import {Router} from '@angular/router';
 
+interface CollectionCheckbox {
+  id: number;
+  name: string;
+  checked?: boolean;
+}
+
+interface StatusCheckbox {
+  id: number;
+  name: string;
+  checked?: boolean;
+}
+
 @Component({
   selector: 'app-library',
   templateUrl: './library.page.html',
   styleUrls: ['./library.page.scss'],
 })
+
 export class LibraryPage implements OnInit {
   @ViewChild('slideWithButtons') slideWithNav: IonSlides;
   slideOpts = {
@@ -21,9 +34,16 @@ export class LibraryPage implements OnInit {
   private _userId: number;
   private _books: Book[] = [];
   private _currentlyReading: Book[] = [];
-  private _genreList: string[] = [];
+  private _collections: CollectionCheckbox[] = [];
+  private _status: StatusCheckbox[] = [
+    {id: 0, name: 'Unread', checked: true},
+    {id: 1, name: 'Read', checked: true},
+    {id: 2, name: 'Ongoing', checked: true},
+  ];
   private _currentSelection: Book[] = [];
   private _sortOptions: string[] = ['Author', 'Title', 'Rating'];
+  private selectedOwnership;
+  private selectedRatings;
 
   constructor(private booksService: BooksService,
               private tokenStorageService: TokenStorageService,
@@ -43,8 +63,12 @@ export class LibraryPage implements OnInit {
     return this._currentlyReading;
   }
 
-  get genreList(): string[] {
-    return this._genreList;
+  get collections(): CollectionCheckbox[] {
+    return this._collections;
+  }
+
+  get status(): StatusCheckbox[] {
+    return this._status;
   }
 
   get currentSelection(): Book[] {
@@ -61,14 +85,12 @@ export class LibraryPage implements OnInit {
   ionViewWillEnter() {
     this.retrieveUserId();
     this.retrieveBooks();
-    this.retrieveGenreList();
+    this.retrieveCollections();
   }
-
 
   retrieveUserId() {
     this._userId = +this.tokenStorageService.getUserId();
   }
-
 
   retrieveBooks() {
     this.booksService.getBooks(this.userId).subscribe(
@@ -84,8 +106,12 @@ export class LibraryPage implements OnInit {
     );
   }
 
-  retrieveGenreList() {
-    this._genreList = ['Fantasy', 'SF', 'Fiction', 'Non-fiction', 'Short stories'];
+  retrieveCollections() {
+    let strings = ['Fantasy', 'SF', 'Fiction', 'Non-fiction', 'Short stories'];
+    let i: number;
+    for (i = 0; i < strings.length; i++) {
+      this._collections.push({id: i, name: strings[i], checked: true});
+    }
   }
 
   retrieveCurrentlyReading() {
@@ -100,13 +126,14 @@ export class LibraryPage implements OnInit {
     slideView.slidePrev(500);
   }
 
-
-  filter() {
-    this._currentSelection = this.books.filter(book => {
+  searchBarFiltering() {
+    this._currentSelection = this.applyFilters().filter(book => {
       return book.title.toLowerCase().indexOf(this.userInput.toLowerCase()) > -1 ||
         book.authors.toString().toLowerCase().indexOf(this.userInput.toLowerCase()) > -1;
-    })
+    });
+
   }
+
 
   addNewBook() {
     this.actionSheetCtrl.create({
@@ -137,5 +164,98 @@ export class LibraryPage implements OnInit {
 
   showBookDetails(bookId: number) {
     this.router.navigate((['/', 'library', 'show', bookId]));
+  }
+
+  onOwnershipChanged(event: any) {
+    switch (event.detail.value) {
+      case 'all': {
+        this.selectedOwnership = 'all';
+        break;
+      }
+      case 'owned': {
+        this.selectedOwnership = 'owned';
+        break;
+      }
+      case 'notOwned': {
+        this.selectedOwnership = 'notOwned';
+        break;
+      }
+
+    }
+  }
+
+  onRatingsChanged(event: any) {
+    switch (event.detail.value) {
+      case '1': {
+        this.selectedRatings = 1;
+        break;
+      }
+      case '2': {
+        this.selectedRatings = 2;
+        break;
+      }
+      case '3': {
+        this.selectedRatings = 3;
+        break;
+      }
+      case '4': {
+        this.selectedRatings = 4;
+        break;
+      }
+      case '5': {
+        this.selectedRatings = 5;
+        break;
+      }
+    }
+  }
+
+  applyFilters() {
+    let tempBooks = this.books;
+    console.log('filtering');
+    console.log(this.collections);
+
+    switch (this.selectedOwnership) {
+      case 'all':
+        break;
+      case 'owned':
+        tempBooks = tempBooks.filter(book => {
+          return book.owned;
+        });
+        break;
+      case 'notOwned':
+        tempBooks = tempBooks.filter(book => {
+          return !book.owned;
+        });
+        break;
+    }
+
+    if (this.selectedRatings != 1) {
+      tempBooks = tempBooks.filter(book => {
+        return book.rating >= this.selectedRatings;
+      });
+    }
+
+    let selected = this.status.filter((status) => status.checked);
+    let temp2 = [];
+    selected.forEach(status => {
+      let temp = tempBooks.filter(book => {
+        console.log(book.status.toLowerCase(), status.name.toLowerCase());
+        return book.status.toLowerCase() === status.name.toLowerCase();
+      });
+      temp.forEach(book => {
+        temp2.push(book);
+      });
+    });
+    tempBooks = temp2;
+
+
+    // selected = this.collections.filter((collection) => collection.checked);
+    // temp2 = [];
+    // récupérer un array de collections et leurs books id, et push les id dans array puis filter les books par id
+    // tempBooks = temp2;
+
+
+    this._currentSelection = tempBooks;
+    return tempBooks;
   }
 }
