219e727e670f506bdbaa73b770b62123c568b169
GwenRspl <gwenaelle.rispal@gmail.com>
2019-06-04 15:15:00 +0200

    add menu burger for medium to small devices

--- src/main/ui/src/app/app.component.ts
+++ src/main/ui/src/app/app.component.ts
@@ -1,19 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+
+  constructor(private platform: Platform,
+              private splashScreen: SplashScreen,
+              private statusBar: StatusBar) {
     this.initializeApp();
   }
 
@@ -23,4 +22,5 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
 }
